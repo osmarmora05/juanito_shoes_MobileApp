74538aee3d74158310d1d96aa2d32c2a066df0ee
osmarmora05 <osmaradrianmoracer@gmail.com>
2024-03-24 21:05:47 -0600

    chore: add StyledTextInput & icons svg

--- .docs/es/GUIDE.md
+++ .docs/es/GUIDE.md
@@ -1,3 +1,8 @@
-# Instalacion de fuentes
+# lib
+
+## Instalacion de fuentes
 - [expo](https://docs.expo.dev/versions/latest/sdk/font/#are-you-using-this-library-in-a)
 - [video](https://www.youtube.com/watch?v=25pG-T7jbGM)
+
+## Importar icono en svg
+- [npm](https://www.npmjs.com/package/react-native-svg-transformer)
---
+++ src/components/ui/StyledTextInput.jsx
@@ -0,0 +1,74 @@
+import { TextInput, View, StyleSheet, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import Eye from "../../../assets/icons/eye/eye.svg";
+import EyeOff from "../../../assets/icons/eye/eye-off.svg";
+import { theme } from "../../theme";
+
+export default function StyledTextInput({
+  emailAddress,
+  phone,
+  password,
+  ...restOfProps
+}) {
+  if (
+    (emailAddress && phone) ||
+    (emailAddress && password) ||
+    (phone && password)
+  ) {
+    throw new Error(
+      "hey! in StyledTextInput Only one type of input is allowed :V"
+    );
+  }
+  const [showPassword, setShowPassword] = useState(false);
+  const toggleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        secureTextEntry={password ? !showPassword : undefined}
+        keyboardType={
+          emailAddress ? "email-address" : phone ? "phone-pad" : "default"
+        }
+        {...restOfProps}
+      />
+      <TouchableOpacity onPress={toggleShowPassword} style={styles.icon}>
+        {password && (
+          <TouchableOpacity onPress={toggleShowPassword}>
+            {showPassword ? (
+              <EyeOff stroke={theme.colors.text.hint} styles={styles.icon} />
+            ) : (
+              <Eye stroke={theme.colors.text.hint} styles={styles.icon} />
+            )}
+          </TouchableOpacity>
+        )}
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    maxWidth: 320,
+    width: "100%",
+    height: 48,
+    borderRadius: 14,
+    paddingLeft: 16,
+    paddingRight: 45,
+    backgroundColor: theme.colors.bg.defaultDark,
+  },
+  icon: {
+    position: "absolute",
+    right: 60,
+    width: 24,
+    height: 24,
+  },
+});
