b91721c37c9cc29a28c0b53cb05ca497ada14226
FranciscoMelen10 <fmelendezsimplina@gmail.com>
2024-06-06 09:39:04 -0600

    Add: Diseño del carrito

---
+++ src/components/CartCard.jsx
@@ -0,0 +1,77 @@
+import { View, StyleSheet, TouchableOpacity, Image, Text } from "react-native";
+import Trash from "../../assets/icons/Trash.svg";
+import StyledText from "./ui/StyledText";
+import { theme } from "../theme";
+import QuantityOfProducts from "./QuantityOfProducts";
+
+export default function CartCard(props) {
+  const { item } = props;
+
+  return (
+    <View style={styles.mainBox}>
+      <View style={styles.box}>
+        <View style={styles.containerimage}>
+          <Image style={styles.image} source={{ uri: `${item.Imagen}` }} />
+        </View>
+        <View style={styles.infoContainer}>
+          <StyledText extraSmall textAlign="left" numberOfLines={1}>
+            {item.Nombre}
+          </StyledText>
+          <StyledText extraSmall bold textAlign="left" numberOfLines={1}>
+            ${item.Precio}
+          </StyledText>
+          <QuantityOfProducts />
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.trashContainer}
+        onPress={() => {
+          console.log("Pronto")
+        }}
+      >
+        <Trash width={14} height={14} />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainBox: {
+    backgroundColor: theme.colors.bg.default,
+    borderRadius: 14,
+    padding: 10,
+  },
+
+  box: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderRadius: 14,
+    gap: 15,
+  },
+
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: "contain",
+    transform: [{ rotate: "-14deg" }],
+  },
+  containerimage: {
+    backgroundColor: theme.colors.bg.defaultDark,
+    borderRadius: 14,
+  },
+
+  trashContainer: {
+    width: 40,
+    height: 40,
+    position: "absolute",
+    top: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  infoContainer: {
+    width: "65%",
+  },
+});
--- src/screens/home/ShoppingCart.jsx
+++ src/screens/home/ShoppingCart.jsx
@@ -1,7 +1,50 @@
-import { Text } from "react-native-svg"
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { useState } from "react";
+import data from "../../data/cart.json";
+import { FlatList } from "react-native-gesture-handler";
+import { theme } from "../../theme";
+import CartCard from "../../components/CartCard";
 
-export default function ShoppingCart() {
-    return(
-        <Text>Hola</Text>
-    )
+export default function ShoppingCart({ navigation }) {
+  const [productData, setProductData] = useState(data);
+
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.productCardContainer}>
+        <CartCard item={item} />
+      </View>
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{data.length} Items</Text>
+      <FlatList
+        data={productData}
+        keyExtractor={(item) => `${item.id}`}
+        renderItem={renderItem}
+        contentContainerStyle={styles.contentContainerStyle}
+      />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colors.bg.defaultDark,
+    padding: 15,
+  },
+  title: {
+    fontSize: 18,
+    color: theme.colors.text.primary,
+    marginLeft: 10,
+  },
+  contentContainerStyle: {
+    alignItems: "center",
+    flexDirection: "column",
+  },
+  productCardContainer: {
+    margin:10,
+  },
+});
